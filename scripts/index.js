8f57586b6848134707f8bc35b0560e4d6edc8cb0
Erwin Heba <erwin.heba@twoupdigital.com>
2022-03-24 20:20:40 +0100

    Exercise-01 - The basics

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+//Strings
+"Text 1";
+'Text 2';
+const textNumer = 3;
+`Text ${textNumer}`;
+
+//Number
+1 - 2.3
+
+//boolean
+true / false
+
+//undefined
+//null
+
+
+//array
+[1, 'str', {}, [], ()];
+
+//object
+{
+    'myFirstElement': 'value',
+    mySecondElement: null
+}
+new Object()
+new Array(10)
