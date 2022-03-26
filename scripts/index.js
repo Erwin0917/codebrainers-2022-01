8cb34c81fca627ac7a0f21ee4480a3fb1a64572c
Robert Wozniczko <schemat2013@gmail.com>
2022-03-26 11:02:51 +0100

    exercise-01 - while

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,16 @@
 //100 - silver button, 1000 - gold, 10000 - diamond
-const rawData = prompt('Please input the amount of subscriptions: ');
-const subscriptions = parseInt(rawData);
-if (isNaN(subscriptions) === true) {
-    console.log('Value should be number ');
+const rawData = prompt("Please input the amount of subscriptions: ");
+let subscriptions = parseInt(rawData);
 
-} else {
+while (isNaN(subscriptions) === true) {
+	console.log("Value should be number ");
+	subscriptions = parseInt(prompt("Please input the amount of subscriptions: "));
+}
 
-
-    if (subscriptions >= 100 && subscriptions < 1000) {
-        console.log('You have achieved silver button');
-
-
-    } else if (subscriptions >= 1000 && subscriptions < 10000) {
-        console.log('You have achieved gold button');
-
-    } else if (subscriptions >= 10000) {
-        console.log('You have achieved diamond button');
-    }
+if (subscriptions >= 100 && subscriptions < 1000) {
+	console.log("You have achieved silver button");
+} else if (subscriptions >= 1000 && subscriptions < 10000) {
+	console.log("You have achieved gold button");
+} else if (subscriptions >= 10000) {
+	console.log("You have achieved diamond button");
 }
