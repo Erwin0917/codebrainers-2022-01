2453d26cda9b9e583aeebbf535cbbbcfedfe8501
Natalia Lukaszka <lukaszkanatalia94@gmail.com>
2022-03-26 12:23:30 +0100

    Exercise-02 - Multiplication table, init

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,39 @@
-//100 - silver button, 1000 - gold, 10000 - diamond
-// const rawData = prompt("Please input the amount of subscriptions: ");
-// let subscriptions = parseInt(rawData);
+// let correctValue = false;
 //
-// while (isNaN(subscriptions) === true) {
-// 	console.log("Value should be number ");
-// 	subscriptions = parseInt(prompt("Please input the amount of subscriptions: "));
-// }
+// while (correctValue === false) {
+//     const rawData = prompt("How many subs are added for a month?");
+//     let answerUser = parseInt(rawData);
+//     if(rawData === null){
+//         break;
+//     }
+//     if (isNaN(answerUser) === true) {
+//         console.log("Please enter correct numberic value!");
+//     } else {
+//         const yearAdd = 12 * answerUser;
 //
-// if (subscriptions >= 100 && subscriptions < 1000) {
-// 	console.log("You have achieved silver button");
-// } else if (subscriptions >= 1000 && subscriptions < 10000) {
-// 	console.log("You have achieved gold button");
-// } else if (subscriptions >= 10000) {
-// 	console.log("You have achieved diamond button");
+//         if (yearAdd >= 100 && yearAdd < 1000) {
+//             console.log("You will achieve silver button");
+//         } else if (yearAdd >= 1000 && yearAdd < 10000) {
+//             console.log("You will achieve gold button");
+//         } else if (yearAdd >= 10000) {
+//             console.log("You will achieve diamond button");
+//         }
+//         correctValue = true;
+//     }
 // }
-// -----------------------
 
-let correctValue = false;
+let x = Math.floor(Math.random() * 10);
+let y = Math.floor(Math.random() * 10);
+const correctAnswer = x * y;
 
-while (correctValue === false) {
-    const rawData = prompt("How many subs are added for a month?");
-    let answerUser = parseInt(rawData);
-    if(rawData === null){
-        break;
-    }
-    if (isNaN(answerUser) === true) {
-        console.log("Please enter correct numberic value!");
-    } else {
-        const yearAdd = 12 * answerUser;
+// const answer = prompt("How much is " + x + "*" + y + "?");
+const answer = prompt(`How much is ${x} * ${y} ?`);
 
-        if (yearAdd >= 100 && yearAdd < 1000) {
-            console.log("You will achieve silver button");
-        } else if (yearAdd >= 1000 && yearAdd < 10000) {
-            console.log("You will achieve gold button");
-        } else if (yearAdd >= 10000) {
-            console.log("You will achieve diamond button");
-        }
-        correctValue = true;
-    }
+if (parseInt(answer) === correctAnswer) {
+    console.log("Your answer is correct");
+} else {
+    console.log("Your answer is not correct");
 }
+
 
 
