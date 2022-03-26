a62bb6ced62bd3b343468fa8f3a36de08fe1bb83
bialypb <99273336+bialypb@users.noreply.github.com>
2022-03-26 10:30:06 +0100

    exercise-01 - isNaN(), NaN

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,20 @@
 //100 - silver button, 1000 - gold, 10000 - diamond
-
-const subscriptions = prompt('Please input the amount of subscriptions: ');
-
-if (subscriptions >= 100 && subscriptions < 1000) {
-    console.log('You have achieved silver button');
-
-
-} else if (subscriptions >= 1000 && subscriptions < 10000) {
-    console.log('You have achieved gold button');
+const rawData = prompt('Please input the amount of subscriptions: ');
+const subscriptions = parseInt(rawData);
+if (isNaN(subscriptions) === true) {
+    console.log('Value should be number ');
 
 } else {
-    console.log('You have achieved diamond button');
+
+
+    if (subscriptions >= 100 && subscriptions < 1000) {
+        console.log('You have achieved silver button');
+
+
+    } else if (subscriptions >= 1000 && subscriptions < 10000) {
+        console.log('You have achieved gold button');
+
+    } else if (subscriptions >= 10000) {
+        console.log('You have achieved diamond button');
+    }
 }
