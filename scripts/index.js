518daa08558a7ae2f963789dae6108f3dfc9e221
Marek Zelek <mzelek89@gmail.com>
2022-03-26 11:56:37 +0100

    exercise-01 - added flag, break in if and while, final solution

--- scripts/index.js
+++ scripts/index.js
@@ -16,24 +16,27 @@
 // }
 // -----------------------
 
-const rawData = prompt("How many subs are added for a month?");
-let answerUser = parseInt(rawData);
-let yearAdd = null;
+let correctValue = false;
 
-if (rawData !== null) {
-    while (isNaN(answerUser) === true) {
-        console.log("Value should be number ");
-        answerUser = parseInt(prompt("How many subs are added for a month?"));
+while (correctValue === false) {
+    const rawData = prompt("How many subs are added for a month?");
+    let answerUser = parseInt(rawData);
+    if(rawData === null){
+        break;
     }
+    if (isNaN(answerUser) === true) {
+        console.log("Please enter correct numberic value!");
+    } else {
+        const yearAdd = 12 * answerUser;
 
-    yearAdd = 12 * answerUser;
-
-    if (yearAdd >= 100 && yearAdd < 1000) {
-        console.log("You have achieved silver button");
-    } else if (yearAdd >= 1000 && yearAdd < 10000) {
-        console.log("You have achieved gold button");
-    } else if (yearAdd >= 10000) {
-        console.log("You have achieved diamond button");
+        if (yearAdd >= 100 && yearAdd < 1000) {
+            console.log("You will achieve silver button");
+        } else if (yearAdd >= 1000 && yearAdd < 10000) {
+            console.log("You will achieve gold button");
+        } else if (yearAdd >= 10000) {
+            console.log("You will achieve diamond button");
+        }
+        correctValue = true;
     }
 }
 
