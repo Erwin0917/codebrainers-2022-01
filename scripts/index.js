9a8c22f1597adeabc4c8388d09dd0f93cfa4b183
borys <borysweiner@gmail.com>
2022-03-26 15:27:30 +0100

    Exercise-03 - sort arrays, compare arrays, callback function; array function

--- scripts/index.js
+++ scripts/index.js
@@ -1,62 +1,106 @@
-// function randomBetween(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-//
-// let goodAnswers = 0;
-// let intQCount = NaN;
-//
-// do {
-//     const qCount = prompt("Enter number of questions:");
-//     intQCount = parseInt(qCount);
-// } while (isNaN(intQCount) === true);
-//
-//
-// for (let i = 0; i < intQCount; i++) {
-//     const x = randomBetween(0, 10);
-//     const y = randomBetween(0, 10);
-//     const correctAnswer = x * y;
-//     const answer = prompt(`How much is ${x} * ${y} ?`);
-//     if (parseInt(answer) === correctAnswer) {
-//         goodAnswers += 1;
-//     }
-// }
-//
-// if (goodAnswers > Math.floor(intQCount * 0.6)) {
-//     console.log(`You passed the test! You had ${goodAnswers} correct answers from ${intQCount}.`);
-// } else {
-//     console.log(`You failed the test! You had ${goodAnswers} correct answers from ${intQCount}.`);
-// }
-
 
 // ---------- Array
 
-const arr = [];
-const nonEmpty = ["Kasia", "Janek", "Zosia"];
-// const arrExample = new Array(5);
+// const arr = [];
+// const nonEmpty = ["Kasia", "Janek", "Zosia"];
+// // const arrExample = new Array(5);
 
-// console.log(nonEmpty.length);
-// console.log(nonEmpty[2]);
-nonEmpty[2] = "Andrzej";
-// console.log(nonEmpty[2]);
-
-
-nonEmpty.push("Kamil", "Anna"); //Dodaje element na końcu tablicy
-const lastElement = nonEmpty.pop(); //Usuwa element z końca tablicy
-
-nonEmpty.unshift("Wojciech"); //Dodaje element na początku tablicy
-const firstElement = nonEmpty.shift(); //Usuwa element z początku tablicy
+// // console.log(nonEmpty.length);
+// // console.log(nonEmpty[2]);
+// nonEmpty[2] = "Andrzej";
+// // console.log(nonEmpty[2]);
 
 
-nonEmpty.push([1, 5, 8]);
-// console.log(nonEmpty[4][2]);
+// nonEmpty.push("Kamil", "Anna"); //Dodaje element na końcu tablicy
+// const lastElement = nonEmpty.pop(); //Usuwa element z końca tablicy
 
-console.log(nonEmpty.indexOf("Bronek"));
-console.log(nonEmpty.includes("Kamil"));
+// nonEmpty.unshift("Wojciech"); //Dodaje element na początku tablicy
+// const firstElement = nonEmpty.shift(); //Usuwa element z początku tablicy
 
-console.log(nonEmpty);
-console.log(nonEmpty.splice(3, 1));
-console.log(nonEmpty);
+
+// nonEmpty.push([1, 5, 8]);
+// // console.log(nonEmpty[4][2]);
+
+// console.log(nonEmpty.indexOf("Bronek"));
+// console.log(nonEmpty.includes("Kamil"));
+
+// console.log(nonEmpty);
+// console.log(nonEmpty.splice(3, 1));
+// console.log(nonEmpty);
+
+
 
 // 1. Wypełnić maszyne 49 piłeczkami.
 // 2. Losować 6 razy jedną piłeczkę.
 // 3. Stworzyć kupon i sprawdzić ile razy będzie losowanie 6tki aż trafimy nasze liczby.
+
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function comparator(a, b) {
+    if (a > b) {
+            return 1;
+        } else if (a < b) {
+            return -1;
+        } else {
+            return 0;
+        }
+}
+
+function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    arr1.sort(comparator);
+    arr2.sort(comparator);
+    let areEqual = true;
+
+    arr1.forEach((currentElement, index)=> {
+        // console.log("element: ", currentElement);
+        // console.log("index: ", index);
+        const arr2Element = arr2[index];
+        if (currentElement != arr2Element) {
+            areEqual = false;
+        }
+    });
+
+    return areEqual;
+}
+
+console.log("Compare resutls: ", compareArrays([1, 2, 3], [3, 2, 1]))
+
+const availableNumbers = []
+
+for (let i = 1; i < 16; i++) {
+    availableNumbers.push(i);
+}
+
+const drawnNumbers = [];
+
+function letsDraw() {
+    for (let i = 1; i < 4; i++) {
+        const drawnIndex = randomBetween(0, availableNumbers.length - 1);
+        drawnNumbers.push(availableNumbers[drawnIndex]);
+        availableNumbers.splice(drawnIndex, 1);
+    }
+}
+
+// const coupon = [4, 12, 29, 44, 13, 23];
+const coupon = [4, 12, 15];
+
+let jackpot = false;
+let numOfDraws = 0;
+
+while (jackpot === false) {
+    letsDraw();
+    numOfDraws += 1;
+    console.log(numOfDraws)
+    if (compareArrays(drawnNumbers, coupon) === true) {
+        jackpot = true;
+    }
+}
+
+console.log(`We had to draw ${numOfDraws} times!`);
+
+// It still doesn't seem to finish
