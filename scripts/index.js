36a1847989358b954e4b1deebc6e56410d242828
Krzysztof Zielinski <zielkin@hotmail.com>
2022-03-26 10:10:04 +0100

    exercise-01 - prompt, if / else, &&

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,14 @@
-//Strings
-// "Text 1";
-// 'Text 2';
-// const textNumer = 3;
-// `Text ${textNumer}`;
-//
-// //Number
-// 1 - 2.3
-//
-// //boolean
-// true / false
-//
-// //undefined
-// //null
-//
-//
-// //array
-// [1, 'str', {}, [], ()];
-//
-// //object
-// {
-//     'myFirstElement': 'value',
-//     mySecondElement: null
-// }
-// new Object()
-// new Array(10)
+//100 - silver button, 1000 - gold, 10000 - diamond
 
-// -----------------------
+const subscriptions = prompt('Please input the amount of subscriptions: ');
 
-function functionName(attr1 = 10){
-    console.log('FunctionName');
-    console.log(attr1);
-    if(attr1 === undefined) {
-        console.error('attr1 is wrong');
-    }
+if (subscriptions >= 100 && subscriptions < 1000) {
+    console.log('You have achieved silver button');
+
+
+} else if (subscriptions >= 1000 && subscriptions < 10000) {
+    console.log('You have achieved gold button');
+
+} else {
+    console.log('You have achieved diamond button');
 }
-
-const arrowFunction = (attr2) => {
-    console.log('arrowFunction');
-    console.log(attr2)
-}
-
-// functionName('10');
-// arrowFunction('new attr');
-
-
-// --------------------
-var oldOne;
-let newFirst;
-const newSecond = 10;
-// newSecond = 20;
-
-const thrVar = [];
-thrVar.push('stg');
-
-// ---------------
-
-// if (10 == '10'){
-//
-// }
-//
-// if (10 === '10'){
-//
-// }
-
-console.log(parseInt('10.2'))
-console.log(parseFloat('10.2'))
-
