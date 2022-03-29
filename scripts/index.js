9609c43449cf1bcc0fe88bf9bf7ce1368a542961
borys <borysweiner@gmail.com>
2022-03-29 18:54:37 +0200

    exercise-04: map method

--- scripts/index.js
+++ scripts/index.js
@@ -5,4 +5,11 @@
 
 const arr = ['ala', 13, 'kasia', 'basia', 5, 'szymek', 'karol', 2];
 
+const newArr = arr.map((element) => {
+    if (typeof element === "number") {
+        return element ** 2;
+    }
+    return element;
+})
 
+console.log(newArr)
