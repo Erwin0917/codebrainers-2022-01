baead7a0a7a2ec3fd1e0819b2b05f161ff1c3a9a
Maks <maks0047@gmail.com>
2022-03-31 18:55:17 +0200

    exercise-04-Maksymilian-L - homework

--- scripts/index.js
+++ scripts/index.js
@@ -39,7 +39,7 @@ map(squareNumber, arr);
 
 const arrayFilter = (array) => (callback) => filter(callback, array);
 const arrBeforeFilter = arrayFilter(arr);
-console.log(arrBeforeFilter(isNumber));
+// console.log(arrBeforeFilter(isNumber));
 
 function filter(callback, array) {
     const newArray = [];
@@ -53,11 +53,75 @@ function filter(callback, array) {
     return newArray;
 }
 
-console.log(filter(isNumber, arr));
+// console.log(filter(isNumber, arr));
 
 
 //TODO: 1. Przefiltrować tablice arr żeby zostały same imiona.
+exerciseCounter = 1;
+console.log(`Zadanie nr ${exerciseCounter++}:`);
+
+function isString(element) {
+    if (typeof element === 'string') {
+        return element;
+    }
+    // if (Array.isArray(element)) {
+    //     for (let i = 0; i < element.length; i++) {
+    //         if (isString(element[i])){
+    //             return element[i];
+    //         }
+    //         delete element[i]
+    //     }
+    // }
+};
+
+console.log("Strings: {", arr.filter(isString),"}\n");
+
+
 //TODO: 2. Usunąć wszystkie wartości false => NaN, undefined, null
+console.log(`Zadanie nr ${exerciseCounter++}:`);
+
+function removeFalse(element, index, array) {
+
+    if (Array.isArray(element) === true) {
+        // console.log("removeFalse()", element.filter(removeFalse))
+        // array[index] = element.filter(removeFalse);
+        return element.filter(removeFalse);
+    }
+
+    if (typeof element === "string") {
+        return element;
+    }
+    if (element === false || isNaN(element) || element === undefined || element === null) {
+        return false;
+    }
+
+    return element;
+};
+
+console.log('Removed false elements: ', arr.map(removeFalse).filter(removeFalse), "\n");
+
+
 //TODO: 3. Zwrócić taka samą tablice ale imiona mają być z wielkiej litery
+console.log(`Zadanie nr ${exerciseCounter++}:`)
+
+function upperCase(element) {
+    if (typeof element === 'string') {
+        return (element[0].toUpperCase() + element.substring(1, element.length));
+    }
+    return element;
+};
+
+console.log('Names from upper case plus rest of table: ', arr.map(upperCase));
+
+
 //TODO: 4. Zwrócić nową tablice tylko z imionami posortowanymi alfabetycznie
+console.log(`Zadanie nr ${exerciseCounter++}:`);
+
+arrayOfNames = arr.filter(isString).sort();
+
+console.log('Names sorted: ',arrayOfNames);
+
+
 //TODO: 5. Zwrócić nową tablice z numerami od największego do najmniejszego
+console.log(`Zadanie nr ${exerciseCounter++}:`);
+
