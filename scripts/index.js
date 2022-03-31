22c1a5913a01eaaccf04fd28f7482bf51bd2b0a2
Erwin Heba <erwin.heba@twoupdigital.com>
2022-03-31 19:57:50 +0200

    Exercise-05 - Objects, Class

--- scripts/index.js
+++ scripts/index.js
@@ -1,97 +1,50 @@
-// [].forEach((element, index) => {})
-// const newArr = [].map((element, index) => {})
-// [].sort((prevElement, nextElement) => {return /number/})
-// const filteredArr = [].filter((element, index) => {return /boolean/})
-
-const arr = ['ala', 13, undefined, 'kasia', 'basia', null, 5, 'szymek', 'karol', 2, NaN, false, ['car', 10, undefined]];
-
-const squareNumber = (element) => {
-    if (typeof element === 'number') {
-        return square(element);
+class Car {
+    constructor(color = 'black', maximumSpeed = 230, speed) {
+        this.color = color;
+        this.maximumSpeed = maximumSpeed;
+        this.numberOfGears = 6;
+        this.currentSpeed = speed;
     }
-    return element;
-};
 
-const isNumber = element => typeof element === 'number' && isNaN(element) === false;
-
-const onlyNumber = arr.filter(isNumber);
-const newArr = arr.map(squareNumber);
-
-const chainMethods = arr.filter(isNumber).map(squareNumber);
-
-// console.log(onlyNumber)
-
-function square(number) {
-    return number ** 2;
-}
-
-function map(callback, array) {
-    const newArray = [];
-    for (let i = 0; i <= array.length - 1; i++) {
-        const elem = array[i];
-        const index = i;
-        newArray.push(callback(elem, index, array));
+    getColor() {
+        return this.color;
     }
-    return newArray;
-}
 
-map(squareNumber, arr);
-
-const arrayFilter = (array) => (callback) => filter(callback, array);
-const arrBeforeFilter = arrayFilter(arr);
-console.log(arrBeforeFilter(isNumber));
-
-function filter(callback, array) {
-    const newArray = [];
-    for (let i = 0; i <= array.length - 1; i++) {
-        const value = array[i];
-        const index = i;
-        if (callback(value, index, array) === true) {
-            newArray.push(value);
+    setColor(newColor) {
+        if (typeof newColor === 'string') {
+            this.color = newColor;
+        } else {
+            console.warn('new color should be string');
         }
     }
-    return newArray;
-}
 
-console.log(filter(isNumber, arr));
-
-
-// DONE: 1. Przefiltrować tablice arr żeby zostały same imiona.
-
-const isString = element => typeof element === "string";
-
-const namesOnlyArr = arr.filter(isString);
-
-console.log('namesOnlyArr', namesOnlyArr);
-
-// DONE: 2. Usunąć wszystkie wartości false => NaN, undefined, null
-// ew. DONE: usunąć również undefined z tablicy w tablicy
-
-const checkForArrs = (element) => {
-    if (Array.isArray(element)) {
-        return  element.filter(element => element);
+    setSpeed = (speed) => {
+        this.currentSpeed = speed;
     }
-    return element;
+
+    accelerate(howMuch) {
+
+    }
+
+    decelerate(howMuch) {
+
+    }
+
 }
 
-const noFalsesArr = arr.map(checkForArrs).filter(element => element)
 
-console.log('noFalsesArr', noFalsesArr);
+const firstCar = new Car('red');
 
-// DONE: 3. Zwrócić taka samą tablice ale imiona mają być z wielkiej litery
+const secondCar = new Car('black');
 
-const capitalized = (arg) => isString(arg) ?  arg[0].toUpperCase() + arg.slice(1) : arg;
-
-const arrWithCapNames = arr.map(capitalized)
-console.log('arrWithCapNames', arrWithCapNames);
+console.log(firstCar);
+console.log(secondCar);
 
 
-// DONE: 4. Zwrócić nową tablice tylko z imionami posortowanymi alfabetycznie
+firstCar.setColor('yellow');
+firstCar.getColor();
 
-const sortedNamesArr = arr.filter(isString).sort();
-console.log(sortedNamesArr);
 
-// DONE: 5. Zwrócić nową tablice z numerami od największego do najmniejszego
+// secondCar.color - get by key
+// secondCar['color']; - get by key
 
-const sortedNumsArr = arr.filter(isNumber).sort((a, b) => b - a);
-console.log('sortedNumsArr', sortedNumsArr)
