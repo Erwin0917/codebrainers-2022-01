4f9c55fc877527ed9d84774ada51a3f8fb1083ac
Erwin Heba <erwin.heba@twoupdigital.com>
2022-04-05 19:09:43 +0200

    Exercise-06 - Class extends

--- scripts/index.js
+++ scripts/index.js
@@ -1,83 +1,49 @@
-function isNumber(arg) {
-    return typeof arg === 'number';
-}
-
-class Car {
-    constructor(color = 'black', maximumSpeed = 230, speed) {
-        this.color = typeof color === 'string' ? color : 'black';
-        this.maximumSpeed = typeof maximumSpeed === 'number' ? maximumSpeed : 230;
-        this.currentSpeed = typeof speed === 'number' ? speed : 0;
-        this.numberOfGears = 6;
+class Person {
+    constructor(hitPoints) {
+        this.hitPoints = hitPoints;
+        this.strength = 0;
     }
 
-    getColor() {
-        return this.color;
-    }
-
-    setColor(newColor) {
-        if (typeof newColor === 'string') {
-            this.color = newColor;
-        } else {
-            console.warn('new color should be string');
-        }
-    }
-
-    setSpeed = (speed) => {
-        if (speed >= this.maximumSpeed) {
-            this.currentSpeed = this.maximumSpeed;
-        } else if (speed <= 0) {
-            this.currentSpeed = 0;
-        } else {
-            this.currentSpeed = speed;
-        }
-    };
-
-    accelerate(howMuch) {
-        if (isNumber(howMuch)) {
-            const newSpeed = this.currentSpeed + howMuch;
-            this.setSpeed(newSpeed);
-        } else {
-            console.warn('Invalid "howMuch" type. Should be a number.');
-        }
-    }
-
-    decelerate(howMuch) {
-        if (isNumber(howMuch)) {
-            const newSpeed = this.currentSpeed - howMuch;
-            this.setSpeed(newSpeed);
-        } else {
-            throw new Error('Invalid "howMuch" type. Should be a number.');
-            console.warn('Invalid "howMuch" type. Should be a number.');
-        }
+    isAlive() {
+        return this.hitPoints > 0;
     }
 
 }
 
+class Hero extends Person {
+    constructor(hitPoints) {
+        super(hitPoints);
+    }
 
-const firstCar = new Car('red');
+    isAlive() {
+        return super.isAlive() && this.strength > 0;
+    }
+}
 
-const secondCar = new Car('black');
-
-console.log(firstCar);
-// console.log(secondCar);
-
-firstCar.accelerate(60);
-console.log('1, current speed should be 60:', firstCar);
-firstCar.accelerate(250);
-console.log('2, current speed should be max speed:', firstCar);
-firstCar.decelerate(200);
-
-try {
-
-} catch (error) {
-    // console.log(error);
+class Villain extends Person {
+    constructor(hitPoints) {
+        super(hitPoints);
+    }
 }
 
 
 
+const firstPerson = new Person('20');
+const firstHero = new Hero('50');
 
-// secondCar.color - get by key
-// secondCar['color']; - get by key
+firstPerson.isAlive();
+firstHero.isAlive();
+
+console.log(firstPerson);
+console.log(firstHero);
+
+console.log(firstHero instanceof Person);
+console.log(firstHero instanceof Hero);
+console.log(firstPerson instanceof Hero);
 
 
-
+function checkCharacter(person) {
+    if (person instanceof Hero) {
+        person.isAlive();
+    }
+}
