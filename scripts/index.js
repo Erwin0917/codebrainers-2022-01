4c97fcf3702b6d52dbbceeb86d2605ae10ae315d
borys <borysweiner@gmail.com>
2022-04-05 20:30:49 +0200

    exercise-06: armorRating attribute, changed attack method to reflect armor rating

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,31 @@
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 class Person {
     constructor(hitPoints) {
         this.hitPoints = hitPoints;
         this.strength = 0;
+        this.armorRating = 0;
 
     }
 
     attack(character, power) {
-        character.hitPoints -= power
+        if (!(character instanceof Person)) {
+            console.error("'Character' is not an instance of 'Person'");
+            return;
+        }
+        const attackQuality = randomBetween(1, 20);
+        if (attackQuality < character.armorRating) {
+            power = power * 0.1;
+        }
+        const healthPool = character.hitPoints - power;
+        if (healthPool < 0) {
+            character.hitPoints = 0;
+        } else {
+            character.hitPoints = healthPool;
+        }
+        console.log("The hit delivered:", power);
     }
 
     isAlive() {
@@ -18,6 +37,7 @@ class Person {
 class Hero extends Person {
     constructor(hitPoints) {
         super(hitPoints);
+        this.armorRating = 10;
     }
 
     isAlive() {
@@ -28,12 +48,17 @@ class Hero extends Person {
 class Villain extends Person {
     constructor(hitPoints) {
         super(hitPoints);
+        this.armorRating = 10;
     }
 }
 
 const firstHero = new Hero(50);
 const darkCharacter = new Villain(40);
 
-console.log(darkCharacter)
-firstHero.attack(darkCharacter, 10);
+while (darkCharacter.isAlive()) {
+    firstHero.attack(darkCharacter, 10);
+    console.log("HIT! Hero attacked darkCharacter!");
+}
+console.log("Dark Character looks pretty beat up:", darkCharacter);
+
 
