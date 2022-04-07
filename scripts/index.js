046d1c22022eb04941090e45bf27308f9bd016cd
borys <borysweiner@gmail.com>
2022-04-07 18:30:54 +0200

    exercise-06: homework solution

--- scripts/index.js
+++ scripts/index.js
@@ -11,22 +11,27 @@ class Person {
 
     }
 
-    attack(character, power) {
+    // deleted power from attack method
+    attack(character) {
         if (!(character instanceof Person)) {
             console.error("'Character' is not an instance of 'Person'");
             return;
         }
-        const attackQuality = randomBetween(1, 20);
+        const attackQuality = randomBetween(0, 20) + this.attackModifier();
+        let damage = this.weapon.getDamage()
         if (attackQuality < character.armorRating) {
-            power = power * 0.1;
+            damage = 0;
+        } else if (attackQuality - this.attackModifier() > 19) {
+            damage = damage * 2;
+            console.log("CRITICAL DAMAGE!")
         }
-        const healthPool = character.hitPoints - power;
+        const healthPool = character.hitPoints - damage;
         if (healthPool < 0) {
             character.hitPoints = 0;
         } else {
             character.hitPoints = healthPool;
         }
-        console.log("The hit delivered:", power);
+        console.log(`The hit rolled for ${attackQuality}, including +${this.attackModifier()} attack modifier. Damage was ${damage}.`);
     }
 
     isAlive() {
@@ -41,23 +46,31 @@ class Person {
         console.error("weapon is not instance of class Weapon");
     }
 
+    attackModifier() {
+        const modifiers = [-2, -2, -2, -1, 0, 1, 2, 3, 4, 5];
+        return modifiers[Math.floor(this.strength / 2)]
+    }
+
 }
 
 class Hero extends Person {
     constructor(hitPoints) {
         super(hitPoints);
         this.armorRating = 10;
+        this.strength = 15;
     }
 
-    isAlive() {
-        return super.isAlive() && this.strength > 0;
-    }
+    // commented out, because it doesn't fit my vision
+    // isAlive() {
+    //     return super.isAlive() && this.strength > 0;
+    // }
 }
 
 class Villain extends Person {
     constructor(hitPoints) {
         super(hitPoints);
         this.armorRating = 10;
+        this.strength = 12;
     }
 }
 
@@ -68,25 +81,38 @@ class Weapon {
     }
 
     getDamage() {
-        return this.maxDamage;
+        return randomBetween(this.minDamage, this.maxDamage);
     }
 }
+
+const axe = new Weapon(1, 8);
+const sword = new Weapon(2, 6);
 
 const firstHero = new Hero(50);
 const darkCharacter = new Villain(40);
 
-const axe = new Weapon(randomBetween(2, 5),randomBetween(6,10));
-
 firstHero.setWeapon(axe);
-//TODO: Finish getDamage method
-//TODO: darkCharacter attack firstHero in while
-//TODO: Use Weapon in attack method
+//DONE: Finish getDamage method
+//DONE: darkCharacter attack firstHero in while
+//DONE: Use Weapon in attack method
 
+darkCharacter.setWeapon(sword);
 
-while (darkCharacter.isAlive()) {
-    firstHero.attack(darkCharacter, 10);
-    console.log("HIT! Hero attacked darkCharacter!");
+while (darkCharacter.isAlive() && firstHero.isAlive()) {
+    console.log("Our brave hero attacks!")
+    firstHero.attack(darkCharacter);
+    if (darkCharacter.isAlive()) {
+        console.log("Now defend yourself!")
+        darkCharacter.attack(firstHero);
+    }
 }
-console.log("Dark Character looks pretty beat up:", darkCharacter);
 
+if (darkCharacter.isAlive()) {
+    console.warn("The forces of evil have triumphed!");
+} else {
+    console.warn("You won your first fight.");
+}
 
+// TODO: refactor code
+// TODO: extend mechanics? Add new stuff?
+// TODO: add unarmed attack version
