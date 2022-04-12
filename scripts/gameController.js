caa63c70f0fe57392b659befef3d0e9b4ff0efd5
Marcin Baran <baran.marcin1995@gmail.com>
2022-04-12 20:48:27 +0200

    exercise-07: refactor battle function

--- scripts/gameController.js
+++ scripts/gameController.js
@@ -1,3 +1,5 @@
+import {randomBetween} from "./utilis.js";
+
 export class GameController {
     constructor(uiController) {
         this.teamA = [];
@@ -7,15 +9,26 @@ export class GameController {
 
     battle() {
         while (!isTeamDead(this.teamA) && !isTeamDead(this.teamB)) {
-            this.teamA.forEach((personA, index) => {
-                duel(personA, this.teamB[index]);
-                this.uiController.renderTeams(this.teamA, this.teamB);
-            })
+            if (randomBetween(0, 1) === 0) {
+                this.teamA.forEach((personA, index) => {
+                    duel(personA, findOpponent(this.teamB));
+                    this.uiController.renderTeams(this.teamA, this.teamB);
+                })
+            } else {
+                this.teamB.forEach((personB, index) => {
+                    duel(personB, findOpponent(this.teamA));
+                    this.uiController.renderTeams(this.teamA, this.teamB);
+                })
+            }
         }
         console.log('Team A:', this.teamA);
         console.log('Team B:', this.teamB);
     }
 
+}
+
+const findOpponent = (team) => {
+    return team.find((person) => person.isAlive())
 }
 
 function duel(attacker, victim) {
