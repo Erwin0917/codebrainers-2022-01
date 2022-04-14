a8f0a4cbe3a45e472a678d7dc8790ebdaa115481
borys <borysweiner@gmail.com>
2022-04-14 18:22:52 +0200

    exercise-07 - findOpponent updated with randomness

--- scripts/gameController.js
+++ scripts/gameController.js
@@ -1,3 +1,5 @@
+import {randomBetween} from "./utilis.js";
+
 export class GameController {
     constructor(uiController) {
         this.teamA = [];
@@ -7,15 +9,39 @@ export class GameController {
 
     battle() {
         while (!isTeamDead(this.teamA) && !isTeamDead(this.teamB)) {
-            this.teamA.forEach((personA, index) => {
-                duel(personA, this.teamB[index]);
-                this.uiController.renderTeams(this.teamA, this.teamB);
-            })
+            if (randomBetween(0, 1) === 0) {
+                this.teamA.forEach((personA, index) => {
+                    if (isTeamDead(this.teamB)) {
+                        return;
+                    }
+                    duel(personA, findOpponent(this.teamB));
+                    this.uiController.renderTeams(this.teamA, this.teamB);
+                })
+            } else {
+                this.teamB.forEach((personB, index) => {
+                    if (isTeamDead(this.teamA)) {
+                        return;
+                    }
+                    duel(personB, findOpponent(this.teamA));
+                    this.uiController.renderTeams(this.teamA, this.teamB);
+                })
+            }
         }
         console.log('Team A:', this.teamA);
         console.log('Team B:', this.teamB);
     }
 
+}
+
+const findOpponent = (team) => {
+    let randomOpponent = team[randomBetween(0, team.length - 1)];
+    while (!randomOpponent.isAlive()) { 
+        randomOpponent = team[randomBetween(0, team.length - 1)];
+    }
+    console.log("Random oponent:", randomOpponent);
+    console.log("is he alive?:", randomOpponent.isAlive);
+    return randomOpponent;
+    // return team.find((person) => person.isAlive())
 }
 
 function duel(attacker, victim) {
