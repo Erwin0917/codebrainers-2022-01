e34762118ff6b01f322e372d6b41ed7e9ff4cf6c
borys <borysweiner@gmail.com>
2022-04-19 20:57:57 +0200

    exercise-08 - localstorage.getItem; homework

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
 import { UiController } from './uiController.js';
 import { GameController } from './gameController.js';
 import {characterList, teamGenerator} from "./character.js";
+import {randomBetween} from "./utilis";
+import {weaponGenerator, weaponList} from "./weapon";
 
+const TEAM_A_KEY = "teamA";
+const TEAM_B_KEY = "teamB"
 
 function gameInit() {
     const gameWrapperHtml = document.querySelector('.game-wrapper');
@@ -20,16 +24,37 @@ function gameInit() {
             gameController.teamB = await teamGenerator(enteredTeamCount, characterList);
             uiController.renderTeams(gameController.teamA, gameController.teamB);
 
-            localStorage.setItem("teamA", JSON.stringify(gameController.teamA));
-            localStorage.setItem("teamB", JSON.stringify(gameController.teamB));
+            localStorage.setItem(TEAM_A_KEY, JSON.stringify(gameController.teamA));
+            localStorage.setItem(TEAM_B_KEY, JSON.stringify(gameController.teamB));
     })
 
-    // if (isTeamAlive(gameController.teamB)) {
-    //     console.warn('The forces of evil have triumphed!');
-    // } else {
-    //     console.warn('You won your first fight.');
-    // }
 
+    const savedTeamA = JSON.parse(localStorage.getItem(TEAM_A_KEY));
+    const savedTeamB = JSON.parse(localStorage.getItem(TEAM_B_KEY));
+
+    console.log("TEAM A:", savedTeamA);
+
+    if (savedTeamA !== null && savedTeamB !== null) {
+        gameController.teamA = savedTeamA.map(resurrectCharacters);
+        gameController.teamB = savedTeamB;
+    }
+
+
+}
+
+// TODO: HOMEWORK
+function resurrectCharacters(characterData) {
+    const hitpoints = characterData.hitPoints;
+    const armorRating = characterData.armorRating;
+    const strength = characterData.strength;
+    // const weapon = weaponGenerator(weaponList);
+    const character = listOfCharacters[Math.floor(Math.random() * listOfCharacters.length)];
+    const newChar = new character(hitpoints, armorRating, strength);
+    // newChar.setWeapon(weapon);
+    newChar.setName(characterData.name);
+    newChar.ID = characterData.id;
+    newChar.image = characterData.image;
+    return newChar;
 }
 
 gameInit();
