030fad90e07de526f203505f19de9e5539286285
Weiner <borys.weiner@capgemini.com>
2022-04-20 13:50:11 +0200

    finished ressurectCharacters and reforgeWeapon functions to add items from JSON to classes

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
 import { UiController } from './uiController.js';
 import { GameController } from './gameController.js';
-import {characterList, teamGenerator} from "./character.js";
+import {characterList, teamGenerator, Person, Hero, Villain} from "./character.js";
+import {randomBetween} from "./utilis.js";
+import {Weapon, weaponGenerator, weaponList} from "./weapon.js";
 
+const TEAM_A_KEY = "teamA";
+const TEAM_B_KEY = "teamB";
 
 function gameInit() {
     const gameWrapperHtml = document.querySelector('.game-wrapper');
@@ -19,14 +23,52 @@ function gameInit() {
             gameController.teamA = await teamGenerator(enteredTeamCount, characterList);
             gameController.teamB = await teamGenerator(enteredTeamCount, characterList);
             uiController.renderTeams(gameController.teamA, gameController.teamB);
+
+            localStorage.setItem(TEAM_A_KEY, JSON.stringify(gameController.teamA));
+            localStorage.setItem(TEAM_B_KEY, JSON.stringify(gameController.teamB));
     })
 
-    // if (isTeamAlive(gameController.teamB)) {
-    //     console.warn('The forces of evil have triumphed!');
-    // } else {
-    //     console.warn('You won your first fight.');
-    // }
 
+    const savedTeamA = JSON.parse(localStorage.getItem(TEAM_A_KEY));
+    const savedTeamB = JSON.parse(localStorage.getItem(TEAM_B_KEY));
+
+    console.log("TEAM A:", savedTeamA);
+    console.log("TEAM B:", savedTeamB);
+
+    if (savedTeamA !== null && savedTeamB !== null) {
+        gameController.teamA = savedTeamA.map(resurrectCharacters);
+        console.log("gameController.teamA:", gameController.teamA);
+
+        gameController.teamB = savedTeamB.map(resurrectCharacters);
+        console.log("gameController.teamB:", gameController.teamAB);
+    }
+
+
+}
+
+// TODO: HOMEWORK
+function resurrectCharacters(characterData) {
+    const hitpoints = characterData.hitPoints;
+    const armorRating = characterData.armorRating;
+    const strength = characterData.strength;
+    const weapon = reforgeWeapon(characterData.weapon);
+    const character = characterList[Math.floor(Math.random() * characterList.length)];
+    const newChar = new character(hitpoints, armorRating, strength);
+    newChar.setName(characterData.name);
+    newChar.ID = characterData.ID;
+    newChar.image = characterData.image;
+    newChar.setWeapon(weapon);
+
+    // console.log("Character data:", newChar);
+    return newChar;
+}
+
+function reforgeWeapon(weaponData) {
+    const weaponName = weaponData.name;
+    const weaponMinDamage = weaponData.minDamage;
+    const weaponMaxDamage = weaponData.maxDamage;
+
+    return new Weapon(weaponName, weaponMinDamage, weaponMaxDamage);
 }
 
 gameInit();
